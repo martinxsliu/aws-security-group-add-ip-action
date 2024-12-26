97f9403a9885eeda6c2f1f7d66456ccf6c358ffd
Martin Liu <martin.xs.liu@gmail.com>
2024-12-26 00:33:52 -0800

    Make creds optional

--- src/config.js
+++ src/config.js
@@ -1,10 +1,24 @@
 const core = require("@actions/core");
 const { EC2Client } = require("@aws-sdk/client-ec2");
 
-const region = core.getInput("aws-region", { required: true });
-const accessKeyId = core.getInput("aws-access-key-id", { required: true });
-const secretAccessKey = core.getInput("aws-secret-access-key", { required: true });
+const region = core.getInput("aws-region", { required: false });
+const accessKeyId = core.getInput("aws-access-key-id", { required: false });
+const secretAccessKey = core.getInput("aws-secret-access-key", { required: false });
 const sessionToken = core.getInput("aws-session-token", { required: false });
+
+const config = {};
+if (region) {
+  config.region = region;
+}
+if (accessKeyId && secretAccessKey) {
+  config.credentials = {
+    accessKeyId,
+    secretAccessKey,
+  };
+  if (sessionToken) {
+    config.credentials.sessionToken = sessionToken;
+  }
+}
 
 const groupIds = core
   .getInput("aws-security-group-id", { required: true })
@@ -18,20 +32,10 @@ const toPort = toPortInput.length > 0 ? parseInt(toPortInput) : false;
 const description = core.getInput("description", { required: false });
 const protocol = core.getInput("protocol", { required: false });
 
-const ec2 = new EC2Client({
-  region,
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-    sessionToken,
-  },
-});
+const ec2 = new EC2Client(config);
 core.info(`EC2 client created for region ${region}`);
 
 module.exports = {
-  region,
-  accessKeyId,
-  secretAccessKey,
   groupIds,
   port,
   toPort,
